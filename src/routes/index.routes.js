b37f7020a8f63c0522252461a9b03e24d9512107
Brendo <brendow_prime@hotmail.com>
2023-08-03 12:34:54 -0400

    config incial banco shortly

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,12 +1,8 @@
 import { Router } from "express";
-import gamesRouter from "./games.routes.js";
-import customersRouter from "./customers.routes.js";
-import rentalsRouter from "./rentals.routes.js";
+
 
 const router = Router()
 /* router.use(receitasRouter) */
-router.use(gamesRouter)
-router.use(customersRouter)
-router.use(rentalsRouter)
+
 
 export default router;

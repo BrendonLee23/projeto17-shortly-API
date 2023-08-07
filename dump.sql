14f6959b2947fa73491b67bc57fd4b10908c6a62
Brendo <brendow_prime@hotmail.com>
2023-08-07 14:44:59 -0400

    atualização Dump

--- dump.sql
+++ dump.sql
@@ -147,12 +147,20 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 INSERT INTO public.sessions VALUES (1, 'a0581b2d-5694-48a8-b63b-45773759da68', 2, '2023-08-06 22:17:36.623251');
 INSERT INTO public.sessions VALUES (2, '45ea1660-a7d6-4aac-b74a-f0a9185b395e', 2, '2023-08-06 22:55:45.777989');
+INSERT INTO public.sessions VALUES (3, '4a018175-c336-4a04-b445-7d80b507fb7e', 2, '2023-08-07 12:21:15.916972');
+INSERT INTO public.sessions VALUES (4, 'f31d771d-84e6-425d-85d6-e92c680a805a', 2, '2023-08-07 12:21:46.345709');
+INSERT INTO public.sessions VALUES (5, 'b6f28491-45e0-4e73-86a6-9843d09a402e', 2, '2023-08-07 12:29:17.205054');
+INSERT INTO public.sessions VALUES (6, 'fb4317a6-1889-41e9-aaa8-56f7af889e94', 2, '2023-08-07 12:55:24.920771');
 
 
 --
 -- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: -
 --
 
+INSERT INTO public.urls VALUES (1, 2, 'https://www.youtube.com/watch?v=Qc19jvNFU7o&ab_channel=EiNerd', '04ba0d', 0, '2023-08-07 14:13:54.213122');
+INSERT INTO public.urls VALUES (2, 2, 'https://www.youtube.com/watch?v=Qc19jvNFU7o&ab_channel=EiNerd', '835bbc', 0, '2023-08-07 14:16:19.16692');
+INSERT INTO public.urls VALUES (3, 2, 'https://www.youtube.com/watch?v=Qc19jvNFU7o&ab_channel=EiNerd', '5191f5', 0, '2023-08-07 14:17:50.173712');
+INSERT INTO public.urls VALUES (4, 2, 'https://www.youtube.com/watch?v=Qc19jvNFU7o&ab_channel=EiNerd', 'bf7dc7', 0, '2023-08-07 14:39:44.310417');
 
 
 --
@@ -167,14 +175,14 @@ INSERT INTO public.users VALUES (2, 'brendo', 'brendo@driven.com.br', '$2b$10$DB
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 2, true);
+SELECT pg_catalog.setval('public.sessions_id_seq', 6, true);
 
 
 --
 -- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.urls_id_seq', 1, false);
+SELECT pg_catalog.setval('public.urls_id_seq', 4, true);
 
 
 --
@@ -205,7 +213,7 @@ ALTER TABLE ONLY public.sessions
 --
 
 ALTER TABLE ONLY public.urls
-    ADD CONSTRAINT "urls_newURL_key" UNIQUE ("newURL");
+    ADD CONSTRAINT "urls_newURL_key" UNIQUE ("shortUrl");
 
 
 --
